722d0e96b595b99f8efb19405016e45fd39f4987
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-06-25 01:42:01 -0300

    feat: creation of token net logic

---
+++ blockchain/contracts/Net.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Net is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Net", "NET") {
+        _mint(msg.sender, initialSupply);
+    }
+}
