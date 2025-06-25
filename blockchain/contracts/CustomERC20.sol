88217aeab7841b647b0300ba0a3554882e06321d
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-06-25 01:42:23 -0300

    feat: creation of token erc20 customized template

---
+++ blockchain/contracts/CustomERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CustomERC20 is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address owner) ERC20(name, symbol) {
+        _mint(owner, initialSupply);
+    }
+}
